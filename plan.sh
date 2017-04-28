5a434f9a8d5310e625dd778e8ff387b97329444c
Dan Isla <disla@google.com>
2017-04-28 15:56:15 -0700

    update to plan.sh

--- plan.sh
+++ plan.sh
@@ -1,12 +1,14 @@
 #!/bin/bash -e
 
-# bash ./plan.sh REPO_BRANCH=${parameters.repository_branch} TF_BACKEND_BUCKET=${parameters.tf_backend_bucket} GOOGLE_PROJECT=${parameters.tf_admin_project} TF_VAR_region=${parameters.region} TF_VAR_billing_account=${parameters.billing_account} TF_VAR_org_id=${parameters.org_id} TF_VAR_project_name=${parameters.project_name}
+# bash ./plan.sh CREDENTIALS_FILE=/var/lib/jenkins/terraform-admin.json REPO_BRANCH=${parameters.repository_branch} TF_BACKEND_BUCKET=${parameters.tf_backend_bucket} GOOGLE_PROJECT=${parameters.tf_admin_project} TF_VAR_region=${parameters.region} TF_VAR_billing_account=${parameters.billing_account} TF_VAR_org_id=${parameters.org_id} TF_VAR_project_name=${parameters.project_name}
 
 # Export NAME=VALUE args to env
 while test $# -gt 0; do
   export "$1"
   shift 
 done
+
+export GOOGLE_CREDENTIALS=$(cat $CREDENTIALS_FILE)
 
 cat > backend.tf <<EOF
 terraform {
