d0e7e562a0a479a0b5b3d4c4a7d80f416887a56d
Dan Isla <disla@google.com>
2017-04-28 15:32:59 -0700

    update to plan.sh

--- plan.sh
+++ plan.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-# bash ./plan.sh TF_BACKEND_BUCKET="${parameters.tf_backend_bucket}" GOOGLE_PROJECT="${parameters.tf_admin_project}" TF_VAR_billing_account="${parameters.billing_account}" TF_VAR_org_id="${parameters.org_id}" TF_VAR_project_name="${parameters.project_name}" TF_VAR_gke_admin_password="${parameters.gke_admin_password}"
+# bash ./plan.sh REPO_BRANCH=${parameters.repository_branch} TF_BACKEND_BUCKET="${parameters.tf_backend_bucket}" GOOGLE_PROJECT="${parameters.tf_admin_project}" TF_VAR_billing_account="${parameters.billing_account}" TF_VAR_org_id="${parameters.org_id}" TF_VAR_project_name="${parameters.project_name}" TF_VAR_gke_admin_password="${parameters.gke_admin_password}"
 
 # Export NAME=VALUE args to env
 while test $# -gt 0; do
@@ -12,7 +12,7 @@ cat > backend.tf <<EOF
 terraform {
   backend "gcs" {
     bucket = "${TF_BACKEND_BUCKET}"
-    path   = "$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')/terraform.tfstate"
+    path   = "${REPO_BRANCH}/terraform.tfstate"
   }
 }
 EOF
