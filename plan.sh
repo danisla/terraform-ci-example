6e6f78c07b9e4c2dd0eadfc17d36f9c4485ebbfe
Dan Isla <disla@google.com>
2017-04-28 15:22:26 -0700

    update to plan.sh

--- plan.sh
+++ plan.sh
@@ -1,6 +1,21 @@
-#!/bin/bash -xe
+#!/bin/bash -e
 
-curl -LO https://releases.hashicorp.com/terraform/0.9.4/terraform_0.9.4_linux_amd64.zip
-/usr/bin/unzip terraform_0.9.4_linux_amd64.zip
+# bash ./plan.sh TF_BACKEND_BUCKET="${parameters.tf_backend_bucket}" GOOGLE_PROJECT="${parameters.tf_admin_project}" TF_VAR_billing_account="${parameters.billing_account}" TF_VAR_org_id="${parameters.org_id}" TF_VAR_project_name="${parameters.project_name}" TF_VAR_gke_admin_password="${parameters.gke_admin_password}"
 
-./terraform plan
+# Export NAME=VALUE args to env
+while test $# -gt 0; do
+  export "$1"
+  shift 
+done
+
+cat > backend.tf <<EOF
+terraform {
+  backend "gcs" {
+    bucket = "${TF_BACKEND_BUCKET}"
+    path   = "$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')/terraform.tfstate"
+  }
+}
+EOF
+
+terraform init
+terraform plan
