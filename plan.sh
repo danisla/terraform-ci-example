0720d9567c5ff4332747b62e3f793b154b76fa06
Dan Isla <disla@google.com>
2017-04-28 14:05:12 -0700

    update to plan.sh

--- plan.sh
+++ plan.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -xe
 
 curl -LO https://releases.hashicorp.com/terraform/0.9.4/terraform_0.9.4_linux_amd64.zip
-unzip terraform_0.9.4_linux_amd64.zip
+/usr/bin/unzip terraform_0.9.4_linux_amd64.zip
 
 ./terraform plan
