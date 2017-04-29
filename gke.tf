cef41497b674cb72e28afe36edbb4f46432fbc94
Dan Isla <disla@google.com>
2017-04-29 16:02:48 -0700

    removed node-pools from tf

--- gke.tf
+++ gke.tf
@@ -2,10 +2,6 @@ variable "gke_node_count" {
   default = 3
 }
 
-variable "gke_node_pool_count" {
-  default = 1
-}
-
 resource "random_id" "gke_admin_password" {
   byte_length = 15
 }
@@ -56,14 +52,6 @@ resource "google_container_cluster" "cluster1" {
   }
 
   depends_on = ["google_project_services.project"]
-}
-
-resource "google_container_node_pool" "np1" {
-  project            = "${google_project.project.project_id}"
-  name               = "node-pool-1"
-  zone               = "${data.google_compute_zones.available.names[0]}"
-  cluster            = "${google_container_cluster.cluster1.name}"
-  initial_node_count = "${var.gke_node_pool_count}"
 }
 
 output "cluster_endpoint" {
