99b55c4b9d9ad9cca29733f0e6016572f4620036
Dan Isla <disla@google.com>
2017-04-29 11:25:27 -0700

    support for destroy in build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
 # bash ./plan.sh ACTION=plan CREDENTIALS_FILE=/var/lib/jenkins/terraform-admin.json REPO_BRANCH=${parameters.repository_branch} TF_BACKEND_BUCKET=${parameters.tf_backend_bucket} GOOGLE_PROJECT=${parameters.tf_admin_project} TF_VAR_region=${parameters.region} TF_VAR_billing_account=${parameters.billing_account} TF_VAR_org_id=${parameters.org_id} TF_VAR_project_name=${parameters.project_name}
+# Possible values for ACTION: plan,apply,destroy
 
 # Export NAME=VALUE args to env
 while test $# -gt 0; do
@@ -19,5 +20,7 @@ terraform {
 }
 EOF
 
+[[ ${ACTION,,} == "destroy" ]] && export FORCE_ARG="-force"
+
 terraform init
-terraform ${ACTION} -no-color
+terraform ${ACTION} ${FORCE_ARG} -no-color
