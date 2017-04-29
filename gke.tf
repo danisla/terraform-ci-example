555d58dd8b9bcdbbf1fd5f2949154195e097f285
Dan Isla <disla@google.com>
2017-04-29 15:46:16 -0700

    fix to node-pool

--- gke.tf
+++ gke.tf
@@ -55,8 +55,9 @@ resource "google_container_cluster" "cluster1" {
 }
 
 resource "google_container_node_pool" "np1" {
+  project            = "${google_project.project.project_id}"
   name               = "node-pool-1"
-  zone               = "${google_container_cluster.cluster1.zone}"
+  zone               = "${data.google_compute_zones.available.names[0]}"
   cluster            = "${google_container_cluster.cluster1.name}"
   initial_node_count = 1
 }
