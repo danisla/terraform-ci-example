e05ef9380223b190ce321fdb05d4745e4938bc2a
Dan Isla <disla@google.com>
2017-04-29 19:35:15 -0700

    added depends_on to cluster network

--- network.tf
+++ network.tf
@@ -2,4 +2,6 @@ resource "google_compute_network" "cluster1-network" {
   project = "${google_project.project.project_id}"
   name    = "cluster1-network"
   auto_create_subnetworks = "true"
+
+  depends_on = ["google_project_services.project"]
 }
